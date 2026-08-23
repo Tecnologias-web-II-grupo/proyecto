eb0d5b9e88985182ea4a8d826d8431ef9366ddc7
yultap <yureyesta@est.utn.ac.cr>
2026-08-23 17:23:25 -0600

    ajustes de logica api

--- api-factura/src/server.js
+++ api-factura/src/server.js
@@ -14,8 +14,8 @@ const { calentarNavegador, obtenerEstadoBrowser, cerrarBrowser } = require('../d
 const { obtenerEstadoRenderer } = require('../document-renderer/pdfRenderer');
 
 const app = express();
-const API_VERSION = '4.8.0';
-const TEMPLATE_VERSION = 'factura-v44-ecosistema-servicios-v25';
+const API_VERSION = '4.8.1';
+const TEMPLATE_VERSION = 'factura-v44-ecosistema-servicios-v26';
 
 const allowedOrigins = new Set(
   (process.env.FRONTEND_URL || '')
